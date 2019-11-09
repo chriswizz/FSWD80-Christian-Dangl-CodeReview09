c1e6448c628832dd9c2bb76a646da2b8240300f9
chriswizz <christian.dangl1@gmail.com>
2019-11-09 14:05:31 +0100

    finish version 02 add more queries

--- cr09_christian_dangl_carrental_queries.sql
+++ cr09_christian_dangl_carrental_queries.sql
@@ -1,10 +1,10 @@
-how many customers does the company have?
+--how many customers does the company have?
 
 SELECT COUNT(*)
 FROM customers;
 
 
-how often has a "Mercedes" been rented?
+--how often has a "Mercedes" been rented?
 
 SELECT COUNT(*)
 FROM reservations
@@ -14,7 +14,7 @@ INNER JOIN makes ON models.fk_make_id = makes.make_id
 WHERE makes.make = "Mercedes";
 
 
-who has rented a premium ("P") car?
+--who has rented a premium ("P") car?
 
 SELECT first_name, last_name
 FROM customers
@@ -24,4 +24,26 @@ INNER JOIN models ON cars.fk_model_id = models.model_id
 INNER JOIN categories ON models.fk_category_id = categories.category_id
 WHERE categories.name = "P"
 GROUP BY customers.customer_id;
+
+
+--how many reservations hat additional charges?
+
+SELECT COUNT(*)
+FROM reservations
+INNER JOIN additional_charges ON reservations.fk_additional_charge_id = additional_charges.additional_charge_id
+WHERE additional_charges.price > 0;
+
+
+--how many reservations hat different start/end locations and who are the customers?
+
+SELECT COUNT(*)
+FROM reservations
+WHERE fk_location_start <> fk_location_end;
+
+SELECT customers.first_name, customers.last_name
+FROM customers
+INNER JOIN reservations ON customers.customer_id = reservations.fk_customer_id
+WHERE fk_location_start <> fk_location_end
+GROUP BY customers.customer_id;
+
 
